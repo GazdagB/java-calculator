483f1b63cc774a3898a9bf70ef379fc236365835
GazdagB <gazdagbal@gmail.com>
2025-04-06 15:26:45 +0200

    ✨  App window opens

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,5 +5,16 @@ import javax.swing.*;
 import javax.swing.border.LineBorder;
 
 public class Calculator {
+    int boardWidth = 360;
+    int boardHeight = 540;
 
+    JFrame frame = new JFrame("Calculator");
+
+    Calculator(){
+        frame.setVisible(true);
+        frame.setSize(boardWidth,boardHeight);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+    }
 }
